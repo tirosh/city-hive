4838b953566b95f584e63dfa3a6929b89358d20f
Till Grosch <mail@tillgrosch.com>
2023-11-24 23:36:03 +0100

    Adds devise integration + authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    redirect_to pages_dashboard_path if user_signed_in?
+  end
+
+  def dashboard
+    redirect_to root_path unless user_signed_in?
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Cityhive
+module CityHive
   class Application < Rails::Application
     config.application_name = Rails.application.class.module_parent_name
     # Initialize configuration defaults for originally generated Rails version.
