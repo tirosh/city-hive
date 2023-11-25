e9024b9e4245f36e8ea96549e5b962fa3c77bf8c
Till Grosch <mail@tillgrosch.com>
2023-11-25 12:56:26 +0100

    Adds faker + seeds

--- Gemfile
+++ Gemfile
@@ -51,6 +51,8 @@ group :development do
   # gem "spring"
 
   gem "error_highlight", ">= 0.4.0", platforms: [:ruby]
+
+  gem "faker"
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Clear existing records
+User.destroy_all
+Hobby.destroy_all
+
+# Seed Users
+25.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    city: Faker::Address.city
+  )
+end
+
+# Seed Hobbies
+10.times do
+  Hobby.create!(
+    name: Faker::Hobby.unique.activity
+  )
+end
+
+# Associate Hobbies with Users
+User.all.each do |user|
+  user.hobbies << Hobby.all.sample(rand(1..5))
+end
