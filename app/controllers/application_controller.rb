760b452b0233bfd6c333fc0b2ddc53c823504c71
Till Grosch <mail@tillgrosch.com>
2023-11-25 09:22:03 +0100

    Allows user to choose her hobbies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,30 @@
 class ApplicationController < ActionController::Base
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:first_name, :last_name, :name, :email, :password)
+      u.permit(
+        :first_name,
+        :last_name,
+        :name,
+        :email,
+        :password,
+        hobby_ids: []
+      )
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:first_name, :last_name, :name, :email, :password, :password_confirmation, :current_password)
+      u.permit(
+        :first_name,
+        :last_name,
+        :name, :email,
+        :password,
+        :password_confirmation,
+        :current_password,
+        hobby_ids: []
+      )
     end
   end
-
-
 end
