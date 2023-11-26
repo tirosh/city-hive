01822388935b38d4cdb03af83bec1eefa3a12ca1
Till Grosch <mail@tillgrosch.com>
2023-11-26 08:56:11 +0100

    Adds controller action to find hobby partners

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,13 @@ class PagesController < ApplicationController
   def dashboard
     redirect_to root_path unless user_signed_in?
   end
+
+  # Get an example user with a matching hobby. Preferably from the same city.
+  def find_hobby_partners
+    matched_user = MatchMaker::HobbyMatch.call(current_user)
+
+    respond_to do |format|
+      format.json { render json: matched_user }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/home'
   get 'pages/dashboard'
+  get 'pages/find_hobby_partners', to: 'pages#find_hobby_partners', as: 'find_hobby_partners'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
