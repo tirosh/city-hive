96b315b8b9b29688e7a319c352bf21d61cab004c
Till Grosch <mail@tillgrosch.com>
2023-11-26 08:51:03 +0100

    Adds user-related methods for hobby and city filtering

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,22 @@ class User < ApplicationRecord
   has_person_name
   has_and_belongs_to_many :hobbies, inverse_of: :user
   validates :city, presence: true
+
+  # Fetches users who share at least one hobby with the current user.
+  #
+  # @return [ActiveRecord::Relation] A distinct collection of users with shared hobbies.
+  def users_with_shared_hobbies
+    User.joins(:hobbies)
+        .where(hobbies: { id: hobby_ids })
+        .where.not(id:)
+        .distinct
+  end
+
+  # Fetches users from the same city as the current user.
+  #
+  # @return [ActiveRecord::Relation] A collection of users from the same city.
+  def users_from_same_city
+    User.where(city:)
+        .where.not(id:)
+  end
 end
